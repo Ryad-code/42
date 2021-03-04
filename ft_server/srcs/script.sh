a6a91c4eabef307889ae9b9c0ba494d4f8caa68a
Mohamed-ryad LAOUEDJ <mlaouedj@e2r8p6.42.fr>
2021-03-04 15:48:58 +0100

    ft_server_04/03/21

--- ft_server/srcs/script.sh
+++ ft_server/srcs/script.sh
@@ -1,19 +1,27 @@
 #NGINX
 mkdir var/www/site
-chmod -R 755 /var/www/*
+chmod -R 755 /var/www/site
 service nginx start
 
-#MYSQL
+#SETTING UP SERVER BLOCKS
+mkdir -p /var/www/site/html
+#chown -R $USER:$USER /var/www/site/html
+mv index.html var/www/site/html
 mv site etc/nginx/sites-available
-ln -s /etc/nginx/sites-available/site /etc/nginx/sites-enabled
+ln -s /etc/nginx/sites-available/site /etc/nginx/sites-enabled/
+
+#SSL_CERTIFICATE
+mkdir etc/nginx/ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj ‘/C=FR/ST=12/L=ABC/O=1#/CN=mlaouedj’ -keyout /etc/nginx/ssl/localhost.key -out /etc/nginx/ssl/localhost.crt
+#openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/0=42 School/OU=ryad/CN=localhost"
+
+#MYSQL
+#mv site etc/nginx/sites-available
+#ln -s /etc/nginx/sites-available/site /etc/nginx/sites-enabled
 service mysql start
-mariadb
 echo "CREATE DATABASE site_db;" | mysql -u root
 echo "GRANT ALL ON site_db.* TO 'mlaouedj'@'localhost' IDENTIFIED BY 'password';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
-
-#PHP
-mv info.php var/www/site
 
 #Lancement
 service nginx restart
