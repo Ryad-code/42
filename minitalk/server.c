4a96241abdd4bef24d4dfa5a3be6853c3ff43f3e
mlaouedj <mlaouedj@student.42.fr>
2021-06-28 18:12:46 +0200

    minitalk_28/06/21

--- minitalk/server.c
+++ minitalk/server.c
@@ -5,22 +5,36 @@
 
 void	handler(int a)
 {
+	static int	i;
 	static int	nb;
-	
+	static char	*res;
+
+	if (!res)
+		res = malloc(sizeof(char) * 9);
+	if (!i)
+		i = 0;
 	if (nb == 0)
 		nb = 1;
 	if (a == SIGUSR1)
 	{
-		printf("1");
+		res[i] = '1';
+//		printf("1");
 	}
 	else if (a == SIGUSR2)
-		printf("0");
+	{
+		res[i] = '0';
+//		printf("0");
+	}
+	i++;
 	if (nb % 8 == 0)
-		printf("\n");
+	{
+		printf("%s\n", res);
+		i = 0;
+	}
 	nb++;
 }
 
-int main(int ac, char *av)
+int main(int ac, char **av)
 {
 	printf("%d\n", getpid());
 	signal(SIGUSR2, handler);
